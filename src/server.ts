b3eb7768bfb915eb23cb048ac59772890bc373d4
galib6 <asadullahalgalib81@gmail.com>
2024-01-27 21:54:08 +0600

    d

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,22 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("🗂️🗂️_____Database connection established_____🗂️🗂️");
+    await mongoose.connect(config.database_url as string)
+    console.log('🗂️🗂️_____Database connection established_____🗂️🗂️')
     app.listen(config.port, () => {
       console.log(
         `🎯🎯 Server listening on port ${
           process.env.PORT
-        },🎯🎯 url: ${`http://localhost:${config.port}/`}`
-      );
-    });
+        },🎯🎯 url: ${`http://localhost:${config.port}/`}`,
+      )
+    })
   } catch (err) {
-    console.log("Failed to connect to mongodb");
-    console.log(err);
+    console.log('Failed to connect to mongodb')
+    console.log(err)
   }
 }
 
-bootstrap();
+bootstrap()
